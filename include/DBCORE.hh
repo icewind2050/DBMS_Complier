f308752817eeecb111172a6c37df5c064684f4aa
ZhouHongwei <hongweizhou@foxmail.com>
2023-05-23 14:46:32 +0800

    w

--- include/DBCORE.hh
+++ include/DBCORE.hh
@@ -5,6 +5,7 @@
 #include<vector>
 #include<map>
 #include<memory>
+#include<unordered_map>
 using std::shared_ptr;
 using std::string;
 using std::vector;
@@ -12,6 +13,7 @@ using std::map;
 
 enum TYPE{STRING,INT,FLOAT};
 enum OP{GREAT,LESS,EQUAL,NOEQUAL};
+
 struct selectCondition
 {   
     TYPE datatype;
@@ -19,6 +21,10 @@ struct selectCondition
     string value;
 };
 
+struct selectConditions
+{
+    map<string,selectCondition> con;
+}
 
 struct recordUnit
 {
@@ -33,13 +39,25 @@ class Table
 private:
     vector<vector<recordUnit>> table;
 public:
-    Table(/* args */);
-    shared_ptr<vector<vector<string,string>>> select(const map<string,selectCondition>& con);
+    Table();
+    Table(const vector<vector<recordUnit>>& another);
+    shared_ptr<vector<vector<string,string>>> select(const selectConditions& con);
     bool insert(const vector<recordUnit> & value);
     bool update(const vector<recordUnit> & value);
-    bool 
+    bool deleteSQL(const map<string,selectCondition>& con);
 
     ~Table();
+};
+
+class DataBase
+{
+private:
+    string name;
+    unordered_map<string,Table> Tables;
+public:
+    DataBase();
+    DataBase(string n);
+    ~DataBase();
 };
 
 
---
+++ include/DBSHELL.hh
@@ -0,0 +1,20 @@
+#ifndef _SHELL_H
+#define _SHELL_H
+#include"DBCORE.hh"
+class DBSHELL
+{
+public:
+    DBSHELL(/* args */);
+    ~DBSHELL();
+};
+
+DBSHELL::DBSHELL(/* args */)
+{
+}
+
+DBSHELL::~DBSHELL()
+{
+}
+
+
+#endif
